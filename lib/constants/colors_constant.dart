195d74ef7f5697e25951f45d21c49c7d95378a1d
Ha Nguyen Hung <hanh2@fpts.com.vn>
2022-11-02 17:48:47 +0700

    add color

---
+++ lib/constants/colors_constant.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color colorLightGrayWeb = Color(0xffe7e6e6);
+  static const Color colorTitleBlue = Color(0xff053b83);
+  static const Color colorFPTSBlue = Color(0xff0064B4);
+  static const Color colorNavBarBlue = Color(0xff1d61ac);
+  static const Color colorTextLightBlue = Color(0xff0079ff);
+  static const Color colorFPTSGray = Color(0xff808080);
+  static const Color colorFPTSDarkGray = Color(0xff4D4D4D);
+  static const Color colorFPTSLightGray = Color(0xffC9C9C9);
+  static const Color colorFPTSRed = Color(0xffC2161C);
+  static const Color colorCeilPrice = Color(0xffFF23B4);
+  static const Color colorRefPrice = Color(0xffF0FF3B);
+  static const Color colorFloorPrice = Color(0xff00B9FF);
+  static const Color colorPriceUp = Color(0xff50b849);
+  static const Color colorPriceDown = Color(0xffd71a21);
+  static const Color colorPriceNoChange = Color(0xfff26f21);
+  static const Color colorPlaceHolderColor = Color(0xfff7f7f7);
+  static const Color colorFPTSHeaderLightGray = Color(0xffe5e5e5);
+  static const Color colorCeilPriceWhiteBg = Color(0xffFF23B4);
+  static const Color colorRefPriceWhiteBg = Color(0xffF26F21);
+  static const Color colorFloorPriceWhiteBg = Color(0xff00B9FF);
+  static const Color colorPriceUpWhiteBg = Color(0xff50B748);
+  static const Color colorPriceDownWhiteBg = Color(0xffC2161C);
+  static const Color colorPriceNoChangeWhiteBg = Color(0xffF26F21);
+  static const Color colorFlickering = Color(0xff7898c5);
+  static const Color colorWatchlistBg2 = Color(0xff444444);
+  static const Color colorWatchlistBg = Color(0xff000000);
+  static const Color colorWatchlistBg1 = Color(0xff222222);
+  static const Color colorWatchlistHeader = Color(0xff666666);
+  static const Color colorWatchlistHeader1 = Color(0xff7D7D7D);
+  static const Color colorTextNews = Color(0xff6c747f);
+}
